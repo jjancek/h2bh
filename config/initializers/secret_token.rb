df82e8a8032fc79ee67fb89fe1a946648657d55a
jjancek <jeffjancek@live.com>
2014-05-06 10:39:19 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+H2bh::Application.config.secret_key_base = 'd9eba79d8dafb93eeaf668e5665115a9d03530d39580514e738bb9a83367e513b6e13e147a1686065e5ee42e1b8f5c1a7eabc4722b1cef3cfe2f7a892270e517'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+H2bh::Application.config.session_store :cookie_store, key: '_h2bh_session'
